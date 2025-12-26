f0c5af72c65be5432d35c8fea2454269def04e53
paragon technolgy <patrickcudjoeh@gmail.com>
2025-12-26 22:07:48 +0000

    Fix logo visibility on black navbar pages - add brightness and invert filter

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -196,7 +196,9 @@ export default function SONOFGODNavbar({}: NavbarProps) {
                 alt="SON OF GOD"
                 width={300}
                 height={80}
-                className="h-12 sm:h-14 md:h-16 lg:h-20 w-auto object-contain max-w-[200px] sm:max-w-[250px] md:max-w-[300px] lg:max-w-[350px]"
+                className={`h-12 sm:h-14 md:h-16 lg:h-20 w-auto object-contain max-w-[200px] sm:max-w-[250px] md:max-w-[300px] lg:max-w-[350px] transition-all duration-300 ${
+                  isBlackNavbarPage ? "brightness-0 invert" : ""
+                }`}
                 priority
               />
             </Link>
